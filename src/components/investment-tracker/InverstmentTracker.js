a6a5985d9c2171dfc96a0b5e0530cd86ae649a9c
786muhammedashfaq@gmail.com <786muhammedashfaq@gmail.com>
2023-08-23 12:03:20 +0530

    stocks module completed with responsivelayout

--- src/components/investment-tracker/InverstmentTracker.js
+++ src/components/investment-tracker/InverstmentTracker.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import "./InverstmentTracker.css"
+// import "./InverstmentTracker.css"
 import group from "../../assets/Group.svg"
 import add from "../../assets/add.svg"
 import Lottie from "react-lottie";
@@ -107,31 +107,29 @@ const InverstmentTracker = () => {
             case 'Custom':return <div className="it-right-body"> 
             <h1>{tracker.name}</h1>
                         </div>
-            default:return <div className="it-right-body">
-            <div className="add-tracker">
+            default:return <div className="flex flex-col items-center text-white gap-5">
+            <div className="border border-[#F1CA00] p-5 rounded-2xl w-[50%] flex flex-col items-center	">
                 <div className="heading">Add a tracker</div>
-                <button className="add"><img  src={add}/></button>
+                <img  src={add} />
             </div>
-            <div className="learn-investment">
-                <div>
-                    <div>
-                    <span className="data">Learn</span><br />
-                    <span className="data">How to choose</span><br />
-                    <span className="data">right investment</span><br />
-                    </div>
-                    
-                    <div className="Button__login learn">
-                        <button
-                        >
+            <div className="flex flex-col items-center border border-white rounded-3xl">
+                
+                    <span className="">Learn</span><br />
+                    <span className="">How to choose</span><br />
+                    <span className="">right investment</span><br />
+                   
+                    <div className="relative">
+                        
                             <div style={{ width: 258, height: 45 }}>
                             <Lottie
                                 options={lottieOptions}
+                                label="Learn Now"
                             />
                             </div>
-                            <h3 className="learn-more">Learn Now</h3>
-                        </button>
+                            <h3 className="absolute top-2 left-[35%] text-black font-bold">Learn Now</h3>
+                       
                         </div>
-                </div>
+                
             </div>
     </div>
         }
@@ -152,92 +150,146 @@ const InverstmentTracker = () => {
       };    
     
     return (
-        <div className="it-grid">
+        // <div className="it-grid">
             
-             <div className="it-left-body">
-                <div className="it-body-data">
-                    <div className="net-worth">Net Worth</div>
-                    <div className="data">
-                        <div className="investment">₹ 41.9K</div>
-                        <div className="group">
-                            <div className="persentage">11.5%</div>
-                            <img className="group-img" src={group} />
+        //      <div className="it-left-body">
+        //         <div className="it-body-data">
+        //             <div className="net-worth">Net Worth</div>
+        //             <div className="data">
+        //                 <div className="investment">₹ 41.9K</div>
+        //                 <div className="group">
+        //                     <div className="persentage">11.5%</div>
+        //                     <img className="group-img" src={group} />
+        //                 </div>
+                        
+        //             </div>
+        //             <div className={ !isMobileResolution && changeTracker?.name? "remove" : `it-amount`}>
+        //                 <div className="net-amount">Your net worth increase by ₹12,000 than last month</div>
+        //                 <div className="updated">Last Updated Mar 29,2023</div>
+        //               </div>
+        //         </div>
+        //         <div className="scroll-container">
+        //             {/* <hr  className="hr-tag"></hr> */}
+        //         <div className="it-left-body-footer">   
+        //             {
+        //                 listOfTrackers?.map((item)=>{
+        //                     return(
+        //                         <>
+        //                             <a className={item.name !== changeTracker?.name ? "it-data" : `it-data selected`} onClick={() => {onChangeTracker(item)}}>
+        //                                 <div><img src={item.img} /></div>
+        //                                 <div className="stocks">
+        //                                     <div className="name">{item.name}</div>
+        //                                     <div className="value">{item.values}</div>
+        //                                     <div className="value-1">{item.persentage} <img src={group} /></div>
+        //                                 </div>
+        //                                 <div className="navigation">
+        //                                     <button className="navigate-button"><img src={arrow} /></button>
+        //                                 </div>
+        //                             </a>
+        //                         </>
+        //                     )
+        //                 })
+        //             }
+
+        //         </div>
+        //         </div>
+        //     </div>  
+        //     <div className="side-bar" > <div style={{background:"#141414",zIndex:"20",transition:"width 0.4s ease-out",borderRight:"2px solid #e2e2e2"}}></div></div>
+        //      {
+        //     //    !changeTracker?.name ?
+        //     //     <div className="it-right-body">
+        //     //         <div className="add-tracker">
+        //     //             <div className="heading">Add a tracker</div>
+        //     //             <button className="add"><img  src={add}/></button>
+        //     //         </div>
+        //     //         <div className="learn-investment">
+        //     //             <div>
+        //     //                 <div>
+        //     //                 <span className="data" >Learn</span><br />
+        //     //                 <span className="data">How to choose</span><br />
+        //     //                 <span className="data">right investment</span><br />
+        //     //                 </div>
+                            
+        //     //                 <div className="Button__login learn">
+        //     //                     <button
+        //     //                     >
+        //     //                         <div style={{ width: 258, height: 45 }}>
+        //     //                         <Lottie
+        //     //                             options={lottieOptions}
+        //     //                         />
+        //     //                         </div>
+        //     //                         <h3 className="learn-more">Learn Now</h3>
+        //     //                     </button>
+        //     //                     </div>
+        //     //             </div>
+        //     //         </div>
+        //     // </div>    
+        //     // : 
+        //     // changeTracker?.name === "savings" && 
+        //     //     <div className="it-right-body"> 
+        //     //         <CustomTracker 
+        //     //             subHEading = {"Edit/Manage"}
+        //     //             heading = {"Saving"}
+        //     //             currentValue = {"Current Saving"}
+        //     //             addSaving = {"Add Saving"}
+        //     //             removeSaving = {"Remove Saving"}
+        //     //         /> 
+        //     //     </div>
+        //     }     
+        //     {
+        //         renderTrackerDetail(changeTracker)
+        //     }
+
+        // </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-4 md:p-1 md:pl-40  items-start justify-center">
+            
+             <div className="flex flex-col">
+                <div className="flex flex-row-reverse text-white border border-slate-200 items-center justify-between p-8 rounded-xl">
+                    <div className="">Net Worth</div>
+                    <div className="">
+                        <div className="text-3xl">₹ 41.9K</div>
+                        <div className="border border-green-600">
+                            <div className="inline">11.5%</div>
+                            <img className="inline" src={group} />
                         </div>
                         
                     </div>
-                    <div className={ !isMobileResolution && changeTracker?.name? "remove" : `it-amount`}>
+                    {/* <div className={ !isMobileResolution && changeTracker?.name? "remove" : `it-amount`}>
                         <div className="net-amount">Your net worth increase by ₹12,000 than last month</div>
                         <div className="updated">Last Updated Mar 29,2023</div>
-                      </div>
+                      </div> */}
                 </div>
-                <div className="scroll-container">
+                {/* <div className="flex"> */}
                     {/* <hr  className="hr-tag"></hr> */}
-                <div className="it-left-body-footer">   
+                <div className="flex overflow-scroll w-full md:grid md:grid-rows-2 md:grid-flow-col md:gap-4">   
                     {
                         listOfTrackers?.map((item)=>{
                             return(
-                                <>
-                                    <a className={item.name !== changeTracker?.name ? "it-data" : `it-data selected`} onClick={() => {onChangeTracker(item)}}>
-                                        <div><img src={item.img} /></div>
-                                        <div className="stocks">
-                                            <div className="name">{item.name}</div>
-                                            <div className="value">{item.values}</div>
-                                            <div className="value-1">{item.persentage} <img src={group} /></div>
+                                <div className={item.name !== changeTracker?.name ? "flex m-2 min-w-[80%] bg-slate-700 rounded-2xl	md:w-44" : "flex m-2 min-w-[80%] bg-slate-700 rounded-2xl	md:w-44 border border-[#F1CA00]"} onClick={() => {onChangeTracker(item)}}>
+                                
+                                       <img src={item.img} alt={item.name} className="w-1/2"/>
+                                        <div className="text-white pt-2 pb-2 flex flex-col items-center justify-around">
+                                           <div>
+                                                <div className="text-slate-300">{item.name}</div>
+                                                <div className="text-2xl">{item.values}</div>
+                                            </div>
+                                            <div className="flex items-center gap-1 border border-green-300 rounded-xl p-1 text-sm"><span>{item.persentage}</span><img src={group} /></div>
+                                            <button className=""><img src={arrow} /></button>
+
                                         </div>
-                                        <div className="navigation">
-                                            <button className="navigate-button"><img src={arrow} /></button>
-                                        </div>
-                                    </a>
-                                </>
+                                        
+                                   
+                                </div>
                             )
                         })
                     }
 
                 </div>
-                </div>
+                {/* </div> */}
             </div>  
-            <div className="side-bar" > <div style={{background:"#141414",zIndex:"20",transition:"width 0.4s ease-out",borderRight:"2px solid #e2e2e2"}}></div></div>
-             {
-            //    !changeTracker?.name ?
-            //     <div className="it-right-body">
-            //         <div className="add-tracker">
-            //             <div className="heading">Add a tracker</div>
-            //             <button className="add"><img  src={add}/></button>
-            //         </div>
-            //         <div className="learn-investment">
-            //             <div>
-            //                 <div>
-            //                 <span className="data" >Learn</span><br />
-            //                 <span className="data">How to choose</span><br />
-            //                 <span className="data">right investment</span><br />
-            //                 </div>
-                            
-            //                 <div className="Button__login learn">
-            //                     <button
-            //                     >
-            //                         <div style={{ width: 258, height: 45 }}>
-            //                         <Lottie
-            //                             options={lottieOptions}
-            //                         />
-            //                         </div>
-            //                         <h3 className="learn-more">Learn Now</h3>
-            //                     </button>
-            //                     </div>
-            //             </div>
-            //         </div>
-            // </div>    
-            // : 
-            // changeTracker?.name === "savings" && 
-            //     <div className="it-right-body"> 
-            //         <CustomTracker 
-            //             subHEading = {"Edit/Manage"}
-            //             heading = {"Saving"}
-            //             currentValue = {"Current Saving"}
-            //             addSaving = {"Add Saving"}
-            //             removeSaving = {"Remove Saving"}
-            //         /> 
-            //     </div>
-            }     
+            {/* <div className="side-bar" >
+                 <div style={{background:"#141414",zIndex:"20",transition:"width 0.4s ease-out",borderRight:"2px solid #e2e2e2"}}></div></div> */}
+  
             {
                 renderTrackerDetail(changeTracker)
             }
--- src/components/investment-tracker/trackers/Stocks.js
+++ src/components/investment-tracker/trackers/Stocks.js
@@ -1,12 +1,195 @@
-import React from 'react'
-
+import React ,{useState,useEffect} from 'react'
+import {
+ 
+  Autocomplete, Button
+ 
+} from "@mui/material";
+import TextField from '@mui/material/TextField';
 export default function Stocks() {
-  return (
-    <div className="it-right-body">
-        <div className='text-[#FEC008] font-bold'>Stocks</div>
-        <div className='text-slate-300 flex w-full border border-cyan-50 p-3 font-thin'><span>Stocks Value</span><span>+21k</span> </div>
-        <div>View Your Stocks</div>
+  const [StockName,setStockName]=useState('');
+  const [stockDetail,setStockDetail]=useState();
+  const [stocksNameData,setStocksNameData]=useState([]);
+  const [investedValue,setInvestedValue]=useState();
+
+  const [buyAveragePrice,setBuyAveragePrice]=useState();
+  const [viewStocks,setViewStocks]=useState(false);
+  const [allStocks,setAllStocks]=useState([]);
+  const handleStocks=(e)=>{
+      
+   
+    if(e?.target?.value ){
+      // console.log(e.target.value);
+        fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${e.target.value}&apikey=SDFU2BDW50I9JYIN`)
+        .then(response=>{
+          return response.json();
+        })
+        .then((result)=>{
+          console.log(result);
+          setStocksNameData(result.bestMatches)
+        })
+        .catch(error =>{
+           console.log(error.message)
+          //  setError(error.message)
+        }
+           );
+    }
+ 
+   
+  }
+  const stockOptions = {
+    options: stocksNameData,
+    getOptionLabel: (each) => each['2. name']?each['2. name']:'',
+  };
+  const handleSelectedStock=(e,option)=>{
+    console.log(option)
+    setStockDetail(option)
+    setStockName(option)
+    
+  }
+  const addStock=()=>{
+    if(stockDetail){
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization",`Bearer ${JSON.parse(localStorage.getItem('access_token'))}` );
+      myHeaders.append("Content-Type", "application/json");
+      
+      var raw = JSON.stringify({
+        "stockSymbol": stockDetail['1. symbol'],
+        "stockName": stockDetail['2. name'],
+        "perSharePrice": buyAveragePrice,
+        "numberOfShares": Math.round(investedValue/buyAveragePrice),
+        "totalAmount": investedValue
+      });
+      
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+      
+      fetch("https://findemybackedcode.onrender.com/stock/saveStock", requestOptions)
+        .then(response => response.json())
+        .then(result => {console.log(result)
+        setViewStocks(true)
+        })
+        .catch(error => console.log('error', error));
+      
+    }
+      }
+      useEffect(()=>{
+        var myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${JSON.parse(localStorage.getItem('access_token'))}` );
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://findemybackedcode.onrender.com/stock/viewStocks", requestOptions)
+  .then(response => response.json())
+  .then(result =>{
+    setAllStocks(result?.data)
+  })
+  .catch(error => console.log('error', error));
+      },[viewStocks])
+  return ( <>
+    {!viewStocks?<div className="it-right-body">
+      <div>
+          <div className='text-[#FEC008] font-bold'>Stocks</div>
+          <div className='text-slate-300 flex w-full border border-cyan-50 p-3 font-thin'><span>Stocks Value</span><span>+21k</span> </div>
+          <div><Button variant='contained' onClick={()=>setViewStocks(true)}>View Your Stocks</Button></div>
+        </div>
+        <div className='w-full flex flex-col gap-5'>
+         <Autocomplete
+      
+        {...stockOptions}
+        id="stockSearch"
+        autoComplete
+        onInputChange={handleStocks}
+        onChange={(event,option)=>handleSelectedStock(event,option)}
+        // includeInputInList
+        defaultValue={StockName}
+        value={StockName?StockName:null}
+    
+    // loadingText='Loading'
+        renderInput={(params) => (
+          <TextField {...params}
+
+           InputProps={{...params.InputProps, disableUnderline: true}} 
+           placeholder='Stock Name'
+            variant="outlined"
+            sx={{ color:'white',  
+            "& input": {
+                color: 'white',
+            },
+            "& fieldset": {
+              border: "1px solid white",
+            },
+
+        }}
+         
+          />
+        )}
         
-    </div>
+      />
+   
+       <TextField
+                value={buyAveragePrice}
+                placeholder="Buy Average Price"
+                name="average-price"
+                // margin="dense"
+                disabled={false}
+                type='number'
+                variant='outlined'
+                
+                sx={{ color:'white',  
+                  "& input": {
+                      color: 'white',
+                  },
+                  "& fieldset": {
+                    border: "1px solid white",
+                  },
+
+              }}
+              
+                onChange={(e) => setBuyAveragePrice(e.target.value)  }
+                />
+
+      <TextField
+                value={investedValue}
+                placeholder="Invested Value"
+                name="invested-value"
+                // margin="dense"
+                disabled={false}
+                
+                type='number'
+                variant="outlined"
+                sx={{color:'white',  
+                "& input": {
+                    color: 'white',
+                },
+                "& fieldset": {
+                  border: "1px solid white",
+                },
+               
+
+            }}
+
+                onChange={(e) =>  setInvestedValue(e.target.value) }
+                />
+            <Button onClick={addStock} 
+              variant='outlined' color='success'
+            > Add</Button>
+        </div>
+    </div>:
+  <div className="it-right-body text-white">
+    <Button onClick={()=>setViewStocks(false)} > Go Back</Button>
+   {allStocks?.map(each=>{
+    return <div>{each.stockSymbol}</div>
+   })}
+  </div>
+  }
+  </>
   )
 }
